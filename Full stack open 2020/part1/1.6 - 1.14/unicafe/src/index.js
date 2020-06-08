3d487a28b663454dcaff8197d4f21cd28d9c1d3c
Xleine <919143384@qq.com>
2020-06-08 22:19:56 +0800

    update 1.10: unicafe 步骤5

--- Full stack open 2020/part1/1.6 - 1.14/unicafe/src/index.js
+++ Full stack open 2020/part1/1.6 - 1.14/unicafe/src/index.js
@@ -10,6 +10,14 @@ const Button = props => {
   return <button onClick={ handleClick }>{text}</button>
 }
 
+const Statistic = props => {
+  const { text, value } = props
+
+  return (
+    <p>{text} {value}</p>
+  )
+}
+
 const Statistics = props => {
   const { good, neutral, bad } = props
 
@@ -29,12 +37,12 @@ const Statistics = props => {
   return (
     <div>
       <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average {average}</p>
-      <p>positive {positive}%</p>
+      <Statistic text='good' value={good} />
+      <Statistic text='neutral' value={neutral} />
+      <Statistic text='bad' value={bad} />
+      <Statistic text='all' value={all} />
+      <Statistic text='average' value={average} />
+      <Statistic text='positive' value={positive} />
     </div>
   )
 }
