e2f59457d758ceafc3b8aca24b33bf6e08b895d6
Xleine <919143384@qq.com>
2020-06-08 17:46:18 +0800

    update 1.3: 步骤3，课程信息: 使用Object

--- Full stack open 2020/part1/part1/src/index.js
+++ Full stack open 2020/part1/part1/src/index.js
@@ -6,18 +6,18 @@ const Header = props => {
 }
 
 const Part = props => {
-  const { part, exercises } = props
+  const { name, exercises } = props
 
-  return <p> {part} {exercises} </p>
+  return <p> {name} {exercises} </p>
 }
 const Content = props => {
-  const { part1, exercises1, part2, exercises2, part3, exercises3 } = props
+  const { part1, part2, part3 } = props
 
   return (
     <div>
-      <Part part={part1} exercises={exercises1} />
-      <Part part={part2} exercises={exercises2} />
-      <Part part={part3} exercises={exercises3} />
+      <Part name={part1.name} exercises={part1.exercises} />
+      <Part name={part2.name} exercises={part2.exercises} />
+      <Part name={part3.name} exercises={part3.exercises} />
     </div>
   )
 }
@@ -29,18 +29,24 @@ const Total = props => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Content part1={part1} part2={part2} part3={part3} />
+      <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises} />
     </div>
   )
 }
