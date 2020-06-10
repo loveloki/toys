81718cfed53f237e51fda9078bad8a2b2051fa66
Xleine <919143384@qq.com>
2020-06-10 12:11:28 +0800

    update 2.6: The Phonebook 步骤1

--- Full stack open 2020/part2/Exercises 2.6.-2.10/phonebook/src/components/App/index.js
+++ Full stack open 2020/part2/Exercises 2.6.-2.10/phonebook/src/components/App/index.js
@@ -6,19 +6,30 @@ const App = () => {
   ])
   const [ newName, setNewName ] = useState('')
 
+  const handleInput = e => {
+    setNewName(e.target.value)
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+
+    setPersons(persons.concat({name: newName}))
+    setNewName('')
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleInput} />
         </div>
         <div>
-          <button type="submit">add</button>
+          <button type="submit" onClick={handleSubmit}>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      { persons.map(person => <p key={person.name}>{person.name}</p>) }
     </div>
   )
 }
