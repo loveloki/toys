1f0476f89189b1b2fa2767383def9bbb5c520f0d
Xleine <919143384@qq.com>
2020-06-11 22:33:35 +0800

    update 2.14*: Data for countries, 步骤3

--- Full stack open 2020/part2/Exercises 2.12.-2.14/countries/src/components/Country/index.js
+++ Full stack open 2020/part2/Exercises 2.12.-2.14/countries/src/components/Country/index.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import Axios from 'axios'
 
 const Country = props => {
   const { country } = props
+  const api_key = process.env.REACT_APP_WEATHER_API_KEY
+  const [weather, setWeather] = useState({})
+
+  useEffect(() => {
+    Axios
+      .get(`http://api.weatherstack.com/current?access_key=${api_key}&query=${country.name}`)
+      .then(response => {
+        setWeather(response.data)
+      })
+  }, [])
 
   return (
     <div>
@@ -13,6 +24,17 @@ const Country = props => {
         {country.languages.map(lang => <li key={lang.name}>{lang.name}</li>)}
       </ul>
       <img width={200} src={country.flag} alt={`${country.name}国旗`}/>
+      <h2>Weather</h2>
+      <div>
+        {Object.keys(weather).length === 0
+        ? '等待获取天气...获取不到请检查网络或 weatherstack 的api配置'
+        :
+        <>
+          <p>temperature: {weather.current.temperature}</p>
+          <img src={weather.current.weather_icons} alt={`${country.name} 天气`} />
+          <p>wind: {weather.current.wind_speed} {weather.current.wind_degree} {weather.current.wind_dir}</p>
+        </>}
+      </div>
     </div>
   )
 }
